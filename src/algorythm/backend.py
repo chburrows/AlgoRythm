0fcf27958aa1775e439675fc8efad872fbe2e9bf
cburrows1 <cburrows@ufl.edu>
2021-06-18 19:38:57 -0400

    added basic data visualization in pygame

--- src/algorythm/backend.py
+++ src/algorythm/backend.py
@@ -3,29 +3,42 @@ import numpy as np
 from scipy.fft import rfft, rfftfreq
 import time
 
-FORMAT = pyaudio.paFloat32
+FORMAT = pyaudio.paFloat32 # use float to restrict values between (0, 1)
 CHANNELS = 1 # use 1 channel for now, can try to visualize stereo later
 RATE = 48000 # Audio sample rate, shouldn't need to be adjusted
 CHUNK = 1024 # lower number is less latency, higher number improves performance
 
 KEEP_FRAMES = 100 #number of frames to keep in the list (mostly just need most recent frame)
+
 recent_frames = []
+last_freqs = None
+last_levels = None
+stream = None
+
+p = pyaudio.PyAudio()
 
 def callback(in_data, frame_count, time_info, status):
-    global recent_frames
+    global recent_frames, last_freqs, last_levels
+    
     numpydata = np.frombuffer(in_data, dtype=np.float32)
+    
     yf = np.abs(rfft(numpydata))
     xf = rfftfreq(CHUNK, 1 / RATE)
 
+    #normalization of fft values - other methods could be investigated
     yl = 1.0 / (CHUNK / 2) * yf
+    
     bins = list(zip(xf, yl))
-
     recent_frames.append(bins)
     recent_frames = recent_frames[-1 * KEEP_FRAMES:]
+    
+    last_freqs = xf
+    last_levels = yl
+    
     return (yl,pyaudio.paContinue)
 
 def start_stream():
-    p = pyaudio.PyAudio()
+    global stream,p
 
     for i in range(p.get_device_count()):
         dev = p.get_device_info_by_index(i)
@@ -42,16 +55,17 @@ def start_stream():
     stream.start_stream()
 
 
-    while stream.is_active():
-        #replace this with other logic for visualizer
-        if len(recent_frames) > 0:
-            print(recent_frames[-1][:5])
 
-        time.sleep(0.1)
-
+def stop_stream():
     stream.stop_stream()
     stream.close()
     p.terminate()
 
-#remove this for actual use - only here for testing
-start_stream()
+if __name__ == '__main__':
+    start_stream()
+    while stream.is_active():
+        if len(recent_frames) > 0:
+            print(recent_frames[-1][:5])
+
+        time.sleep(0.1)
+    stop_stream()
---
+++ src/algorythm/graphics.py
@@ -0,0 +1,60 @@
+import pygame
+import backend
+
+class AudioBar:
+    def __init__(self, x, max_height, width, color):
+        self.x = x
+        self.max_height = max_height
+        self.width = width
+        self.color = color
+    def update(self, intensity):
+        #calculate y pos of bar based on normalized intensity from 0 to 1
+        #could add velocity or some smoothing function here instead of directly translating instensity
+        self.draw_y = self.max_height * ( 1 - intensity)
+    def draw(self, screen):
+        #draw the rectangle to the screen using pygame draw rect
+        pygame.draw.rect(screen, self.color, (self.x, self.draw_y , self.width, self.max_height - self.draw_y), 0)
+
+
+pygame.init()
+
+#scale factor = maybe a non constant scale factor could be better
+# it looks like the low end consistently has higher intensity than the high end
+SCALE = 10
+
+size = (700, 500)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("AlgoRythm")
+clock = pygame.time.Clock()
+
+backend.start_stream()
+bars = []
+while len(bars) == 0:
+    if len(backend.recent_frames) == 0:
+        continue
+    # creation of the AudioBar objects and add them to the list
+    # right now theres as many bars as frequencies, but they could be grouped (averaged?) to create fewer bars here
+    bar_width = size[0] / len(backend.last_freqs)
+    for i in range(len(backend.last_freqs)):
+        bars.append(AudioBar(bar_width * i, size[1], bar_width, (255,0,0)))
+
+run = True
+while run:
+    for event in pygame.event.get():
+        #close program if X button clicked
+        if event.type == pygame.QUIT:
+            run = False
+
+    #update bars based on levels - have to adjust if fewer bars are used
+    for i in range(len(backend.last_levels)):
+        bars[i].update(backend.last_levels[i] * SCALE)
+
+    #drawing logic - should be handled mostly in AudioBar draw
+    screen.fill( (0, 0, 0) )
+    for bar in bars:
+        bar.draw(screen)
+
+    pygame.display.flip()
+    clock.tick(60)
+backend.stop_stream()
+pygame.quit()
