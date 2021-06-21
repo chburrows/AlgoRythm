3192b389b16f18596c3e1cb0347d8188a303d12a
Laurence M <laurence.mullen@ufl.edu>
2021-06-21 03:53:25 +0100

    Add loopback capability for output devices

--- src/algorythm/backend.py
+++ src/algorythm/backend.py
@@ -5,8 +5,8 @@ import time
 
 FORMAT = pyaudio.paFloat32 # use float to restrict values between (0, 1)
 CHANNELS = 1 # use 1 channel for now, can try to visualize stereo later
-RATE = 48000 # Audio sample rate, shouldn't need to be adjusted
-CHUNK = 1024 # lower number is less latency, higher number improves performance
+CHUNK = 128 # lower number is less latency, higher number improves performance
+rate = 48000 # Will be set by device
 
 KEEP_FRAMES = 100 #number of frames to keep in the list (mostly just need most recent frame)
 
@@ -23,7 +23,7 @@ def callback(in_data, frame_count, time_info, status):
     numpydata = np.frombuffer(in_data, dtype=np.float32)
     
     yf = np.abs(rfft(numpydata))
-    xf = rfftfreq(CHUNK, 1 / RATE)
+    xf = rfftfreq(CHUNK, 1 / rate)
 
     #normalization of fft values - other methods could be investigated
     yl = 1.0 / (CHUNK / 2) * yf
@@ -40,21 +40,42 @@ def callback(in_data, frame_count, time_info, status):
 def start_stream():
     global stream,p
 
+    # Get default output device
+    dev = p.get_default_output_device_info()
+    def_name = dev['name']
+
+    found_loop = False
+    # Try to find a matching named device using WASAPI
     for i in range(p.get_device_count()):
         dev = p.get_device_info_by_index(i)
-        if ('Stereo Mix' in dev['name'] and dev['hostApi'] == 3):
-            dev_index = dev['index']
+        if dev['name'][:len(def_name)] == def_name:
+            # Check loopback capability
+            waspi_check = (p.get_host_api_info_by_index(dev["hostApi"])["name"]).find("WASAPI") != -1
+            if waspi_check:
+                found_loop = True 
+                break
+    
+    if not found_loop:
+        # Change to prompt to change device in future
+        # For now just use microphone
+        print("Default output does not support loopback! Using default input.")
+        dev = p.get_default_input_device_info()
+
+    print('Using Device:',dev['name'])
+    rate = int(dev['defaultSampleRate'])
+
+    # as_loopback requires modified pyaudio from https://github.com/intxcc/pyaudio_portaudio
+    # Currently only supports Python 3.7, may be able to build newer version
     stream = p.open(format = FORMAT,
                     channels = CHANNELS,
-                    rate = RATE,
+                    rate = rate,
                     input = True,
-                    input_device_index = dev_index,
                     frames_per_buffer = CHUNK,
+                    as_loopback = found_loop,
+                    input_device_index = dev['index'],
                     stream_callback=callback)
 
     stream.start_stream()
-
-
 
 def stop_stream():
     stream.stop_stream()
