1ed882e5a20b9c9a018ba4c205b82d5ea9d189f6
Gabriella Neris <gabriellaneris@ufl.edu>
2021-07-12 17:59:44 -0400

    Create color_schemes.py
    Color scheme generator function created

---
+++ src/algorythm/color_schemes.py
@@ -0,0 +1,28 @@
+from numpy.lib.arraysetops import unique
+from file import function
+from PIL import Image
+import numpy
+
+def generate_colors(img):
+    # convert bytes to RGB values
+    rgb_img = img.convert('RGB')
+    # make data contiguous for ordering purposes
+    arr = numpy.ascontiguousarray(rgb_img)
+    # flatten to manipulate 
+    arr = arr.ravel()
+    # get the unique colors of the array
+    palette, unique_index = numpy.unique(arr, return_inverse=True)
+    # make array 1D as opposed to ND
+    palette = palette.view(arr.dtype).reshape(-1, arr.shape[-1])
+    num_unique = numpy.bincount(unique_index)
+    sorted = numpy.argsort(num_unique)
+    palette = palette[sorted[::-1]]
+    # return top 5 colors in the image
+    return palette[:5]
+
+
+def rgb_to_hex()
+
+
+
+
