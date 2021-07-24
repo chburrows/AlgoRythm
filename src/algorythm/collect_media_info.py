61c8aaaa815405777044f1a14351a56d50e66628
Laurence <laurencemullen99@gmail.com>
2021-07-23 23:54:01 -0400

    Color schemes (#41)
    * Added comments on how to display other obj/txt
    
    * Color palette option and sample color boxes added
    
    * Added color palette rectangles and clicking functionality
    
    * Bug fixes and better dynamic integration of colors
    
    * Fix issues in color_scheme introduced with scroll
    
    Co-authored-by: Gabriella Neris <gabriellaneris@ufl.edu>

--- src/algorythm/collect_media_info.py
+++ src/algorythm/collect_media_info.py
@@ -6,7 +6,6 @@ from io import BytesIO
 import binascii
 import numpy as np
 from scipy import cluster
-import scipy.cluster
 import algorythm.spotipy_implementation as sp
 
 async def winrtapi():
@@ -34,7 +33,6 @@ def collect_title_artist():
         info_dict['genres'] = list(info_dict['genres'])
 
         title_artist = [info_dict['title'], info_dict['artist']]
-
         return title_artist
     else:
         return ["N/A", "N/A"]
@@ -59,16 +57,20 @@ def generate_colors_from_img(img, num_colors):
     ar = np.asarray(rgb_img)
     shape = ar.shape
     ar = ar.reshape(np.product(shape[:2]), shape[2]).astype(float)
-    codes, dist = scipy.cluster.vq.kmeans(ar, NUM_CLUSTERS)
-    vecs, dist = scipy.cluster.vq.vq(ar, codes)         # assign codes
+    codes, dist = cluster.vq.kmeans(ar, NUM_CLUSTERS)
+    vecs, dist = cluster.vq.vq(ar, codes)         # assign codes
     counts, bins = np.histogram(vecs, len(codes))    # count occurrences
-
+    num_colors = len(counts) if len(counts) < num_colors else num_colors
+    
     max_indeces = np.argpartition(counts, -1 * num_colors)[-1 * num_colors:]
-    colors = ["#" + binascii.hexlify(bytearray(int(c) for c in codes[i])).decode('ascii') for i in max_indeces]
+    colors = [binascii.hexlify(bytearray(int(c) for c in codes[i])).decode('ascii') for i in max_indeces]
     return colors
 
-def generate_colors():
+def generate_colors(count=0):
     curr_media_info = collect_title_artist()
+    # Check if no currently playing track was found
+    if curr_media_info == ["N/A", "N/A"] or '' in curr_media_info:
+        return (None, None)
     track_id = sp.search_for_id(*curr_media_info)
     track_img_url = sp.get_album_art(track_id)
     pil_img = get_background_img(track_img_url)
@@ -76,8 +78,9 @@ def generate_colors():
     tempo = float(features['track']['tempo'])
     time_per_beat = 60.0 / tempo # in sec
     time_sig = features['track']['time_signature']
-    colors = generate_colors_from_img(pil_img, time_sig)
+    count = time_sig if count == 0 else count
+    colors = generate_colors_from_img(pil_img, count)
     return {'time_per_beat':time_per_beat*time_sig, 'colors':colors, 'album_art':pil_img}
-
+  
 if __name__ == '__main__':
     print(generate_colors())
